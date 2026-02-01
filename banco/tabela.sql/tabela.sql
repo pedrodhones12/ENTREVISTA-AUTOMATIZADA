7efc8414f4f9071dd11739535c228499b1aacb4b
pedrodhones12 <99297623+pedrodhones12@users.noreply.github.com>
2026-02-01 13:47:57 -0300

    Delete banco/tabela.sql directory

--- banco/tabela.sql/tabela.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE resultados_ea (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    data_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    respostas TEXT,
-    carreira1 VARCHAR(100),
-    carreira2 VARCHAR(100),
-    carreira3 VARCHAR(100)
-);
-\
